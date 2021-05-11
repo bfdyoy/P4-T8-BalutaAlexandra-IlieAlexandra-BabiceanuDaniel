2ff9d39a2f468ed0f66da69a1e8d72c7bf26abcc
AlexandraGabrielaIlie <ilie.alexandra.m9m@student.ucv.ro>
2021-05-11 18:04:00 +0300

    Added MVC for Admin, Home and User

---
+++ ArtClubManager/ArtClubManager/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArtClubManager.Controllers
+{
+    public class AdminController
+    {
+    }
+}
---
+++ ArtClubManager/ArtClubManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtClubManager.Controllers
+{
+    [Authorize(Roles = "Membru,Non-Membru")]
+    public class UserController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult secondPage()
+        {
+            return View();
+        }
+    }
+}
